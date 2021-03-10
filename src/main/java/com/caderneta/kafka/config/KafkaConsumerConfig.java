e4e19254c156021616f73c157516bb131c9cc661
siron azevedo <sirondba@gmail.com>
2021-03-09 22:56:39 -0300

    Ajuste config docker e config kafka

--- src/main/java/com/caderneta/kafka/config/KafkaConsumerConfig.java
+++ src/main/java/com/caderneta/kafka/config/KafkaConsumerConfig.java
@@ -1,27 +1,45 @@
 package com.caderneta.kafka.config;
 
-//@EnableKafka
-//@Configuration
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import com.caderneta.model.dto.ContaDTO;
+
+@EnableKafka
+@Configuration
 public class KafkaConsumerConfig {
 
-//	@Value(value = "${kafka.bootstrapAddress}")
-//    private String bootstrapAddress;
-//	
-//	private static final String GROUP_ID = "caderneta_account";
-//
-//    @Bean
-//    public ConcurrentKafkaListenerContainerFactory<String, ContaDTO> accountListenerContainerFactory() {
-//        ConcurrentKafkaListenerContainerFactory<String, ContaDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
-//        factory.setConsumerFactory(accountConsumerFactory());
-//        return factory;
-//    }
-//    
-//    private ConsumerFactory<String, ContaDTO> accountConsumerFactory() {
-//        Map<String, Object> props = new HashMap<>();
-//        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-//        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
-//        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-//        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "50");
-//        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(ContaDTO.class, false));
-//    }
+	@Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+	
+	private static final String GROUP_ID = "caderneta_account";
+
+    @Bean
+    @Profile("docker")
+    public ConcurrentKafkaListenerContainerFactory<String, ContaDTO> accountListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, ContaDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(accountConsumerFactory());
+        return factory;
+    }
+    
+    private ConsumerFactory<String, ContaDTO> accountConsumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "50");
+        return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), new JsonDeserializer<>(ContaDTO.class, false));
+    }
 }
--- src/main/java/com/caderneta/kafka/consumer/AccountConsumer.java
+++ src/main/java/com/caderneta/kafka/consumer/AccountConsumer.java
@@ -1,15 +1,28 @@
 package com.caderneta.kafka.consumer;
 
-//@Service
-//@Slf4j
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.caderneta.model.dto.ContaDTO;
+import com.caderneta.service.IContaService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Profile("docker")
 public class AccountConsumer {
 
-//	@Autowired
-//	private IContaService service;
-//	
-//	@KafkaListener(topics = "stream_new_account", groupId = "caderneta_account", containerFactory = "accountListenerContainerFactory")
-//	public void consume(ContaDTO message) throws IOException {
-//		log.info(String.format("Consuming stream_new_account, message: %s", message.toString()));
-//		service.create(message);
-//	}
+	@Autowired
+	private IContaService service;
+	
+	@KafkaListener(topics = "stream_new_account", groupId = "caderneta_account", containerFactory = "accountListenerContainerFactory")
+	public void consume(ContaDTO message) throws IOException {
+		log.info(String.format("Consuming stream_new_account, message: %s", message.toString()));
+		service.create(message);
+	}
 }
